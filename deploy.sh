03e09a2be1145790cd5f97d609ce1348750adf92
Leskhan <l.karatayev@mycar.kz>
2022-01-29 21:59:46 +0600

    fix

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:leskhantos/calendar.git main:gh-pages
+git push -f git@github.com:leskhantos/courses.git main:gh-pages
 
 cd -
